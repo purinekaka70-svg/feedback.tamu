f49b336fed1694bca70f76eed3016c89007980ab
Purice mwero <puricekaka@gmail.com>
2026-05-08 21:08:07 +0300

    Configure Firebase for employee login

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,10 @@
-// Optional local Firebase Web App config.
-// Fill this file from Firebase Console > Project settings > Your apps > Web app.
-// For server-managed config, set TAMU_FIREBASE_* environment variables instead.
-window.tamuFirebaseConfig = window.tamuFirebaseConfig || null;
+window.tamuFirebaseConfig = {
+  apiKey: "AIzaSyApi2RjwEXEW2mTwCMiYERKUNAYL8Toddk",
+  authDomain: "tamuexpress-26e4a.firebaseapp.com",
+  databaseURL: "https://tamuexpress-26e4a-default-rtdb.firebaseio.com",
+  projectId: "tamuexpress-26e4a",
+  storageBucket: "tamuexpress-26e4a.firebasestorage.app",
+  messagingSenderId: "202068792631",
+  appId: "1:202068792631:web:8a9b97028ebd7a45af76b2",
+  measurementId: "G-QKL8GLGQ53"
+};
