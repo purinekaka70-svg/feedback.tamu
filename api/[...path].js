3b7c74fe1a227dcb87c23977f7190939dfb3b211
Purice mwero <puricekaka@gmail.com>
2026-05-09 12:31:39 +0300

    Consolidate Vercel API into one function

---
+++ api/[...path].js
@@ -0,0 +1,50 @@
+const { send } = require("../server/_lib/http");
+
+const routes = {
+  "admin/applications": require("../server/admin/applications"),
+  "admin/control": require("../server/admin/control"),
+  "admin/login": require("../server/admin/login"),
+  "auth/logout": require("../server/auth/logout"),
+  "auth/session": require("../server/auth/session"),
+  "cart/index": require("../server/cart"),
+  "categories/delete": require("../server/categories/delete"),
+  "categories/index": require("../server/categories"),
+  "categories/save": require("../server/categories/save"),
+  "employee/orders": require("../server/employee/orders"),
+  "firebase/config": require("../server/firebase/config"),
+  "marketplace/list": require("../server/marketplace/list"),
+  "offers/delete": require("../server/offers/delete"),
+  "offers/save": require("../server/offers/save"),
+  "orders/create": require("../server/orders/create"),
+  "orders/delete": require("../server/orders/delete"),
+  "orders/list": require("../server/orders/list"),
+  "orders/update": require("../server/orders/update"),
+  "payments/index": require("../server/payments"),
+  "products/delete": require("../server/products/delete"),
+  "products/save": require("../server/products/save"),
+  "sellers/login": require("../server/sellers/login"),
+  "sellers/register": require("../server/sellers/register"),
+  "sellers/update": require("../server/sellers/update"),
+  "users/index": require("../server/users")
+};
+
+function routeKey(req) {
+  const queryPath = req.query?.path;
+  const rawPath = Array.isArray(queryPath)
+    ? queryPath.join("/")
+    : String(queryPath || "");
+  return rawPath
+    .replace(/\.php$/i, "")
+    .replace(/^\/+|\/+$/g, "")
+    .replace(/\/index$/i, "/index");
+}
+
+module.exports = async function handler(req, res) {
+  const key = routeKey(req);
+  const endpoint = routes[key] || routes[`${key}/index`];
+  if (!endpoint) {
+    send(res, 404, { ok: false, message: "API endpoint was not found." });
+    return;
+  }
+  return endpoint(req, res);
+};
